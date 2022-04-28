a5cbaf472c2107476359c894790c127a52312f94
Fábio de Faria <96897130+fabiomfaria@users.noreply.github.com>
2022-04-28 11:30:55 -0300

    Updated modificações
    simples alterações

--- ormtemplate/settings.py
+++ ormtemplate/settings.py
@@ -4,7 +4,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'MTAwMDUtZmFiaW9t',
+        'PASSWORD': 'Digite sua Senha',
         'HOST': 'localhost',
         'PORT': '5432',
     }
